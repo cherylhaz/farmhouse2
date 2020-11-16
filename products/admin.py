29884b9741704688262caf3785fa9b54b0204f64
cherylhaz <cheryl.hazenberg@gmail.com>
2020-11-16 09:32:10 +0000

    set up product admin and added products

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Product, Category, SubCategory
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'sub_category',
+        'price',
+        'quantity',
+        'image',
+    )
+
+    ordering = ('name',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class SubCategoryAdmin(admin.ModelAdmin): 
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(SubCategory, SubCategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+
+    verbose_name_plural = 'Categories'
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class SubCategory(models.Model):
+
+    verbose_name_plural = 'Sub Categories'
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sub_category = models.ForeignKey('SubCategory', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.DecimalField(max_digits=4, decimal_places=2)
+    shipping_weight = models.DecimalField(max_digits=5, decimal_places=3)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
