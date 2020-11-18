dfc9216046850ccf85920e3ccb37aec92e9fe89f
cherylhaz <cheryl.hazenberg@gmail.com>
2020-11-18 13:42:25 +0000

    working on sorting

--- products/views.py
+++ products/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category, SubCategory
 
 
 def all_products(request):
     """ A view to return the product page """
     products = Product.objects.all()
+    categories= None
+    sub_categories= None
+
+    if request.GET:
+        if sub_category in request.GET:
+            sub_categories = request.GET['sub_category'].split[',']
+            products = products.filter(sub_category__name__in=sub_categories)
+            sub_categories = SubCategory.objects.filter(name__in=sub_categories)
 
     context = {
         'products': products,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
